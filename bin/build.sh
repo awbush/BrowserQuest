a3c83c314fb74593c8257ea28203dec8c916eeee
Anthony Bush <anthony@anthonybush.com>
2012-03-28 20:50:20 -0500

    Fix inability to build in a directory with spaces

--- bin/build.sh
+++ bin/build.sh
@@ -13,7 +13,7 @@ rm -rf $BUILDDIR
 echo "Building client with RequireJS"
 cd $PROJECTDIR
 node ../../bin/r.js -o build.js
-cd $CURDIR
+cd "$CURDIR"
 
 echo "Removing unnecessary js files from the build directory"
 find $BUILDDIR/js -type f -not \( -name "game.js" -o -name "home.js" -o -name "log.js" -o -name "require-jquery.js" -o -name "modernizr.js" -o -name "css3-mediaqueries.js" -o -name "mapworker.js" -o -name "detect.js" -o -name "underscore.min.js" -o -name "text.js" \) -delete
@@ -25,6 +25,6 @@ echo "Removing config directory"
 rm -rf $BUILDDIR/config
 
 echo "Moving build.txt to current dir"
-mv $BUILDDIR/build.txt $CURDIR
+mv $BUILDDIR/build.txt "$CURDIR"
 
 echo "Build complete"
